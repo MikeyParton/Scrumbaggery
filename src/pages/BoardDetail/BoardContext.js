4de60c1d79c418797f5e33f97ea8ac06702d6063
Mike Parton <mikeparton@live.com.au>
2018-05-30 22:15:27 +1000

    Main functions moved over to provider

--- src/pages/BoardDetail/BoardContext.js
+++ src/pages/BoardDetail/BoardContext.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import { graphql, compose } from 'react-apollo'
-import { BOARD_DETAIL_QUERY } from 'data'
+import { crossParentReorder, reorder, addToList, removeFromList } from 'utils/list'
+import {
+  BOARD_DETAIL_QUERY,
+  ADD_LIST_MUTATION,
+  MOVE_LIST_MUTATION,
+  DELETE_LIST_MUTATION,
+  ADD_CARD_MUTATION,
+  MOVE_CARD_MUTATION,
+  BoardDetailPageData
+} from 'data'
 
 const BoardContext = React.createContext({
   boardDetailQuery: () => {}
@@ -9,12 +18,169 @@ const BoardContext = React.createContext({
 export const BoardConsumer = BoardContext.Consumer
 
 class BoardProviderBase extends React.Component {
+
+  addList = (values) => {
+    const { boardDetailQuery: { board }, addListMutation } = this.props
+    addListMutation({
+      variables: {
+        board_id: board.id,
+        name: values.name
+      },
+      optimisticResponse: {
+        __typename: "Mutation",
+        create_list: {
+          __typename: "List",
+          name: values.name,
+          id: Math.round(Math.random() * -1000000),
+          cards: []
+        }
+      },
+      update: (store, response) => {
+        const list = response.data.create_list
+        store.writeQuery({
+          query: BOARD_DETAIL_QUERY,
+          data: {
+            board: {
+              ...board,
+              lists: [
+                list,
+                ...board.lists
+              ]
+            }
+          }
+        })
+      }
+    })
+  }
+
+  moveList = ({ fromIndex, toIndex }) => {
+    const { boardDetailQuery: { board }, moveListMutation } = this.props
+    const list = board.lists[fromIndex]
+    const data = { board: { ...board, lists: reorder(board.lists, fromIndex, toIndex) }}
+
+    moveListMutation({
+      variables: {
+        id: list.id,
+        position: toIndex
+      },
+      optimisticResponse: {
+        __typename: "Mutation",
+        move_list: {
+          __typename: "Board",
+          id: board.id
+        }
+      },
+      update: (store, response) => {
+        store.writeQuery({ query: BOARD_DETAIL_QUERY, data })
+      }
+    })
+  }
+
+  deleteList = (id) => {
+    const { boardDetailQuery: { board }, deleteListMutation } = this.props
+    deleteListMutation({
+      variables: { id },
+      optimisticResponse: {
+        __typename: "Mutation",
+        delete_list: {
+          __typename: "List",
+          id
+        }
+      },
+      update: (store, response) => {
+        store.writeQuery({
+          query: BOARD_DETAIL_QUERY,
+          data: {
+            board: {
+              ...board,
+              lists: board.lists.filter(list => list.id != id)
+            }
+          }
+        })
+      }
+    })
+  }
+
+  addCard = (values) => {
+    const { boardDetailQuery: { board }, addCardMutation } = this.props
+
+    addCardMutation({
+      variables: {
+        list_id: values.list_id,
+        name: values.name
+      },
+      optimisticResponse: {
+        __typename: "Mutation",
+        create_card: {
+          __typename: "Card",
+          name: values.name,
+          id: Math.round(Math.random() * -1000000),
+        }
+      },
+      update: (store, response) => {
+        const card = response.data.create_card
+
+        const list = store.readFragment({
+          id: `List:${values.list_id}`,
+          fragment: BoardDetailPageData.fragments.list,
+          fragmentName: 'BoardDetailPageList'
+        })
+
+        store.writeFragment({
+          id: `List:${values.list_id}`,
+          fragment: BoardDetailPageData.fragments.list,
+          data: { ...list, cards: [card, ...list.cards] },
+          fragmentName: 'BoardDetailPageList'
+        });
+      }
+    })
+  }
+
+  moveCard = ({ fromListIndex, fromIndex, toListIndex, toIndex }) => {
+    const { boardDetailQuery: { board }, moveCardMutation } = this.props
+    const card = board.lists[fromListIndex].cards[fromIndex]
+    const toList = board.lists[toListIndex]
+
+    const newLists = crossParentReorder({
+      lists: board.lists,
+      listKey: 'cards',
+      fromListIndex,
+      fromIndex,
+      toListIndex,
+      toIndex
+    })
+
+    moveCardMutation({
+      variables: {
+        id: card.id,
+        position: toIndex,
+        list_id: toList.id
+      },
+      optimisticResponse: {
+        __typename: "Mutation",
+        move_card: {
+          __typename: "Card",
+          id: card.id
+        }
+      },
+      update: (store, response) => {
+        const data = { board: { ...board, lists: newLists }}
+        store.writeQuery({ query: BOARD_DETAIL_QUERY, data })
+      }
+    })
+  }
+
   render() {
     const { boardDetailQuery, children } = this.props
 
     return (
       <BoardContext.Provider value={{
-        boardDetailQuery
+        boardDetailQuery,
+        addList: this.addList,
+        moveList: this.moveList,
+        deleteList: this.deleteList,
+        addCard: this.addCard,
+        moveCard: this.moveCard
       }}>
         {children}
       </BoardContext.Provider>
@@ -26,5 +192,10 @@ export const BoardProvider = compose(
   graphql(BOARD_DETAIL_QUERY, {
     name: 'boardDetailQuery',
     options: (props) => ({ variables: { id: 1 }})
-  })
+  }),
+  graphql(ADD_LIST_MUTATION, { name: 'addListMutation' }),
+  graphql(MOVE_LIST_MUTATION, { name: 'moveListMutation' }),
+  graphql(DELETE_LIST_MUTATION, { name: 'deleteListMutation' }),
+  graphql(ADD_CARD_MUTATION, { name: 'addCardMutation' }),
+  graphql(MOVE_CARD_MUTATION, { name: 'moveCardMutation' })
 )(BoardProviderBase)
--- src/pages/BoardDetail/BoardDetail.js
+++ src/pages/BoardDetail/BoardDetail.js
@@ -1,16 +1,5 @@
 import React from 'react'
 import { BoardProvider, BoardConsumer } from './BoardContext'
-import { graphql, compose } from 'react-apollo'
-import { crossParentReorder, reorder, addToList, removeFromList } from 'utils/list'
-import {
-  ADD_LIST_MUTATION,
-  DELETE_LIST_MUTATION,
-  ADD_CARD_MUTATION,
-  BOARD_DETAIL_QUERY,
-  MOVE_LIST_MUTATION,
-  MOVE_CARD_MUTATION,
-  BoardDetailPageData
-} from 'data'
 import Button from 'components/Button/Button'
 import Board from 'components/Board/Board'
 import AddCard from 'components/AddCard/AddCard'
@@ -30,166 +19,20 @@ class BoardDetailPage extends React.Component {
     this.setState({ addingCardToList: id })
   }
 
-  onAddList = (values) => {
-    const { boardDetailQuery: { board }, addListMutation } = this.props
-    addListMutation({
-      variables: {
-        board_id: board.id,
-        name: values.name
-      },
-      optimisticResponse: {
-        __typename: "Mutation",
-        create_list: {
-          __typename: "List",
-          name: values.name,
-          id: Math.round(Math.random() * -1000000),
-          cards: []
-        }
-      },
-      update: (store, response) => {
-        const list = response.data.create_list
-        store.writeQuery({
-          query: BOARD_DETAIL_QUERY,
-          data: {
-            board: {
-              ...board,
-              lists: [
-                list,
-                ...board.lists
-              ]
-            }
-          }
-        })
-      }
-    })
-    this.setAddingList(false)
-  }
-
-  onDeleteList = (id) => {
-    const { boardDetailQuery: { board }, deleteListMutation } = this.props
-    deleteListMutation({
-      variables: { id },
-      optimisticResponse: {
-        __typename: "Mutation",
-        delete_list: {
-          __typename: "List",
-          id
-        }
-      },
-      update: (store, response) => {
-        store.writeQuery({
-          query: BOARD_DETAIL_QUERY,
-          data: { board: { ...board, lists: board.lists.filter(list => list.id != id) }}
-        })
-      }
-    })
-  }
-
-  onAddCard = (values) => {
-    const { boardDetailQuery: { board }, addCardMutation } = this.props
-
-    addCardMutation({
-      variables: {
-        list_id: values.list_id,
-        name: values.name
-      },
-      optimisticResponse: {
-        __typename: "Mutation",
-        create_card: {
-          __typename: "Card",
-          name: values.name,
-          id: Math.round(Math.random() * -1000000),
-        }
-      },
-      update: (store, response) => {
-        const card = response.data.create_card
-
-        const list = store.readFragment({
-          id: `List:${values.list_id}`,
-          fragment: BoardDetailPageData.fragments.list,
-          fragmentName: 'BoardDetailPageList'
-        })
-
-        store.writeFragment({
-          id: `List:${values.list_id}`,
-          fragment: BoardDetailPageData.fragments.list,
-          data: { ...list, cards: [card, ...list.cards] },
-          fragmentName: 'BoardDetailPageList'
-        });
-      }
-    })
-    this.setAddingCardToList(null)
-  }
-
-  onMoveList = ({ fromIndex, toIndex }) => {
-    const { boardDetailQuery: { board }, moveListMutation } = this.props
-    const list = board.lists[fromIndex]
-    const data = { board: { ...board, lists: reorder(board.lists, fromIndex, toIndex) }}
-
-    moveListMutation({
-      variables: {
-        id: list.id,
-        position: toIndex
-      },
-      optimisticResponse: {
-        __typename: "Mutation",
-        move_list: {
-          __typename: "Board",
-          id: board.id
-        }
-      },
-      update: (store, response) => {
-        store.writeQuery({ query: BOARD_DETAIL_QUERY, data })
-      }
-    })
-  }
-
-  onMoveCard = ({
-    fromListIndex,
-    fromIndex,
-    toListIndex,
-    toIndex
-   }) => {
-    const { boardDetailQuery: { board }, moveCardMutation } = this.props
-    const card = board.lists[fromListIndex].cards[fromIndex]
-    const toList = board.lists[toListIndex]
-
-    const newLists = crossParentReorder({
-      lists: board.lists,
-      listKey: 'cards',
-      fromListIndex,
-      fromIndex,
-      toListIndex,
-      toIndex
-    })
-
-    moveCardMutation({
-      variables: {
-        id: card.id,
-        position: toIndex,
-        list_id: toList.id
-      },
-      optimisticResponse: {
-        __typename: "Mutation",
-        move_card: {
-          __typename: "Card",
-          id: card.id
-        }
-      },
-      update: (store, response) => {
-        const data = { board: { ...board, lists: newLists }}
-        store.writeQuery({ query: BOARD_DETAIL_QUERY, data })
-      }
-    })
-  }
-
   render() {
     const { addingCardToList, addingList } = this.state
 
     return (
       <BoardProvider>
         <BoardConsumer>
-          {({ boardDetailQuery: { loading, error, board } }) => {
+          {({
+            boardDetailQuery: { loading, error, board },
+            addList,
+            moveList,
+            deleteList,
+            addCard,
+            moveCard
+          }) => {
             if (loading) return <div>Loading...</div>
             if (error) return <div>Error</div>
 
@@ -206,21 +49,21 @@ class BoardDetailPage extends React.Component {
                 <div style={{ marginTop: 50, display: 'flex', flexGrow: 1 }}>
                   <Board
                     board={board}
-                    onMoveCard={this.onMoveCard}
-                    onMoveList={this.onMoveList}
-                    onDeleteList={this.onDeleteList}
+                    onMoveCard={moveCard}
+                    onMoveList={moveList}
+                    onDeleteList={deleteList}
                     onAddCardToList={this.setAddingCardToList}
                   />
                   <AddCard
                     open={Boolean(addingCardToList)}
                     listId={addingCardToList}
                     onClose={() => this.setAddingCardToList(null)}
-                    onAddCard={this.onAddCard}
+                    onAddCard={addCard}
                   />
                   <AddList
                     open={addingList}
                     boardId={board.id}
-                    onSubmit={this.onAddList}
+                    onSubmit={addList}
                     onClose={() => this.setAddingList(false)}
                   />
                 </div>
@@ -233,24 +76,4 @@ class BoardDetailPage extends React.Component {
   }
 }
 
-export default compose(
-  graphql(ADD_LIST_MUTATION, {
-    name: 'addListMutation'
-  }),
-  graphql(DELETE_LIST_MUTATION, {
-    name: 'deleteListMutation'
-  }),
-  graphql(ADD_CARD_MUTATION, {
-    name: 'addCardMutation'
-  }),
-  graphql(MOVE_LIST_MUTATION, {
-    name: 'moveListMutation'
-  }),
-  graphql(MOVE_CARD_MUTATION, {
-    name: 'moveCardMutation'
-  }),
-  graphql(BOARD_DETAIL_QUERY, {
-    name: 'boardDetailQuery',
-    options: { variables: { id: 1 }}
-  })
-)(BoardDetailPage)
+export default BoardDetailPage
